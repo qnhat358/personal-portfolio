ac55778358f873cb48e427a1ee8f30e98d680c3b
nhat.tran-duy-quang <nhat.tran-duy-quang@logi-vietfrance.com>
2024-09-10 19:17:08 +0700

    add skills

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="padding-container">
+  <div class="padding-container column justify-center">
     <p class="fontsize-23 q-mb-xl">I'm a Software Engineer.</p>
     <p>
       A passionate frontend developer based in Da Nang with a love for creating
@@ -9,9 +9,56 @@
       ranging from business websites to complex cross-platform enterprise
       systems.
     </p>
+    <div class="skills column flex-center q-col-gutter-y-sm q-mt-xl">
+      <div class="row q-gutter-md">
+        <q-btn round>
+          <q-img src="~assets/vue.png" width="25px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/quasar.svg" width="30px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/tailwindcss.svg" width="30px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/vuetify.svg" width="30px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/bootstrap.svg" width="30px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/laravel.svg" width="25px" />
+        </q-btn>
+      </div>
+      <div class="row q-gutter-md">
+        <q-btn round>
+          <q-img src="~assets/nodejs.svg" width="30px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/csharp.png" width="30px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/javascript.png" width="23px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/css.png" width="30px" />
+        </q-btn>
+        <q-btn round>
+          <q-img src="~assets/html.svg" width="22px" />
+        </q-btn>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.skills {
+  .q-btn {
+    background-color: $primary-lighter !important;
+    height: 44px;
+    width: 44px;
+  }
+}
+</style>
