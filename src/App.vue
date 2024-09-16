c15b3a4e37a801e3ccd33ed0cdedd99a5c04075a
nhat.tran-duy-quang <nhat.tran-duy-quang@logi-vietfrance.com>
2024-09-16 17:52:30 +0700

    update thumbnail image

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,4 @@
   <router-view />
 </template>
 
-<script setup>
-import { useQuasar } from 'quasar';
-
-const myIcons = {
-  'flag-en': 'img:/images/flag-icons/flag-en.svg',
-  'flag-fr': 'img:/images/flag-icons/flag-fr.svg',
-  'flag-vi': 'img:/images/flag-icons/flag-vi.svg'
-};
-
-const $q = useQuasar();
-
-$q.iconMapFn = (iconName) => {
-  const icon = myIcons[iconName];
-  if (icon !== void 0) {
-    return { icon: icon };
-  }
-};
-</script>
+<script setup></script>
